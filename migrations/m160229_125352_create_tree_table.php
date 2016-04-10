b51324a9de116d2674e778cae7c467f11e0d20f4
jaclise <jaclise.che@gmail.com>
2016-04-10 23:48:24 +0800

    fix key error

--- migrations/m160229_125352_create_tree_table.php
+++ migrations/m160229_125352_create_tree_table.php
@@ -59,7 +59,8 @@ class m160229_125352_create_tree_table extends Migration
                         (9, 9, 1, 2, 0, '公司性质', '', true, true, false, false, false, true, false, false, false, false, false, false, false, 'I_'),
                         (10, 10, 1, 2, 0, '熟练程度', '', true, true, false, false, false, true, false, false, false, false, false, false, false, 'J_'),
                         (11, 11, 1, 2, 0, '工作性质', '', true, true, false, false, false, true, false, false, false, false, false, false, false, 'K_'),
-                        (12, 12, 1, 2, 0, '语言', '', true, true, false, false, false, true, false, false, false, false, false, false, false, 'L_')
+                        (12, 12, 1, 2, 0, '语言', '', true, true, false, false, false, true, false, false, false, false, false, false, false, 'L_'),
+                        (13, 13, 1, 2, 0, '所在地区', '', true, true, false, false, false, true, false, false, false, false, false, false, false, 'M_')
                         ");
     }
 
